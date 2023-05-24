8b78cc1e6440be2a6052c95c68cece75dd4328e5
IvanidzeKutnyk <62153875+IvanidzeKutnyk@users.noreply.github.com>
2023-05-24 20:53:31 +0300

    Initial commit

---
+++ Source/Shadow_Weaver/Shadow_WeaverGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Shadow_WeaverGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHADOW_WEAVER_API AShadow_WeaverGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
