f672909ce018f6204e0f96ca72c2070b36b49668
IvanidzeKutnyk <62153875+IvanidzeKutnyk@users.noreply.github.com>
2023-05-27 21:58:12 +0300

    Add Character Movement

--- Source/Shadow_Weaver/Public/MainPlayer.h
+++ Source/Shadow_Weaver/Public/MainPlayer.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Components/TimelineComponent.h"
 #include "MainPlayer.generated.h"
 
 UCLASS()
@@ -9,8 +10,10 @@ class SHADOW_WEAVER_API AMainPlayer : public ACharacter
 {
 	GENERATED_BODY()
 
+
 public:
 
+	
 	AMainPlayer();
 
 protected:
@@ -22,4 +25,32 @@ public:
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	FTimeline WalkRunTimeline;
+
+public:
+	/*	Смерть Персонажа	*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bDead;
+
+	/*	Селфіпалка	*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		class USpringArmComponent* CameraBoom;
+
+	/*	Камера	*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+		class UCameraComponent* FollowCamera;
+
+	void MoveForwardBackward(float _Axis); // Рух вперед назад
+	void MoveLeftRight(float _Axis); // Рух вліво вправо
+	void Jump();
+	void StopJumping();
+	void SprintStart();
+	void SprintEnd();
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float fMaxWalkSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float fMaxRunSpeed;
 };
