5fe5509d40a3aea03508006f82f5d0eda401ce6d
IvanidzeKutnyk <62153875+IvanidzeKutnyk@users.noreply.github.com>
2023-06-21 23:05:49 +0300

    Add ActorClass

--- Source/Shadow_Weaver/GameObjects/GameActors/PickableActor.cpp
+++ Source/Shadow_Weaver/GameObjects/GameActors/PickableActor.cpp
@@ -1,22 +1,50 @@
 #include "PickableActor.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 APickableActor::APickableActor()
+	: m_InHalfHeight(50) //CapsuleComponent.SIZE
+	, m_InRadius(50)		//CapsuleComponent.SIZE
 {
-
 	PrimaryActorTick.bCanEverTick = true;
 
+	//Capsule Component
+	CapsuleComponent = CreateDefaultSubobject< UCapsuleComponent > (TEXT("CapsuleComponent"));
+	CapsuleComponent->InitCapsuleSize(this->m_InRadius, this->m_InHalfHeight);
+	CapsuleComponent->SetupAttachment(RootComponent);
+	CapsuleComponent->OnComponentBeginOverlap.AddDynamic(this, &APickableActor::OnOverlapBegin);
+	CapsuleComponent->OnComponentEndOverlap.AddDynamic(this, &APickableActor::OnOverlapEnd);
+
+
+	MeshComponent = CreateDefaultSubobject < UStaticMeshComponent > (TEXT("MeshComponent"));
+	MeshComponent->AttachToComponent(CapsuleComponent, FAttachmentTransformRules::KeepRelativeTransform);
 }
 
 void APickableActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 void APickableActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void APickableActor::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor && (OtherActor != this) && OtherComp)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Overlap Begin"));
+	}
+}
+
+void APickableActor::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (OtherActor && (OtherActor != this) && OtherComp)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Overlap End"));
+	}
 }
 
--- Source/Shadow_Weaver/GameObjects/GameActors/PickableActor.h
+++ Source/Shadow_Weaver/GameObjects/GameActors/PickableActor.h
@@ -18,4 +18,30 @@ protected:
 public:	
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION()
+		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+			class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+			class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+private:
+	float m_InRadius;
+	float m_InHalfHeight;
+
+	enum class TYPES
+	{
+		MONEY,
+		ARMOR,
+		WEAPON,
+		RESOURCE
+	};
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+		class UCapsuleComponent* CapsuleComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+		class UStaticMeshComponent* MeshComponent;
 };
